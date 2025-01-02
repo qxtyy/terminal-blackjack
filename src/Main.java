4e66ceb6126ffafad04ad8801a75343719f76914
qxtyy <smolfirez@gmail.com>
2025-01-02 00:18:51 -0600

    heyyy we gettin somewhere

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        boolean quit = true;
-        logic player = new logic();
-        while(quit) {
-            player.roll();
-            //hit stand double down quit continue
-            //h s dd q c
-            //how much would you like to bet
+        logic game = new logic();
+        char input;
 
-        }
+        do {
+            game.roll();
+            System.out.print("Enter a character: ");
+            input = in.next().charAt(0);
+            // bet = in.nextInt(); // make this work somehow by making a bet method w/ parameter
 
+            switch (input) {
+                case 'h':
+                    game.hit();
+                    break;
+                case 's':
+                    game.stand();
+                    break;
+                case 'd':
+                    game.doubleDown();
+                    break;
+                case 'q':
+                    System.out.println("bye");
+                    break;
+                case 'c':
+                    game.roll();
+                    break;
+                case 'b':
+                    System.out.println("How much would you like to bet?");
+                default:
+                    System.out.println("Please enter 'h' (hit), 's' (stand), 'd' (double down), 'q' (quit), 'c' (continue), or 'b' (bet)");
+            }
 
+        } while (input != 'q');
 
-
-
-
+        in.close();
     }
 }
